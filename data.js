69670174a4a2d1f51d71498da5e6de9b214cb9f3
anv126 <107947861+anv126@users.noreply.github.com>
2022-06-21 21:10:42 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,92 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0----",
+      "name": "Лаб Электроэнергетики (с номерами)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "Лаб Электроэнергетики",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.28566286012654274,
+          "pitch": -0.030796633375288707,
+          "title": "Нажмите для просмотра информации",
+          "text": "<span style=\"font-size:14.0pt;line-height:115%;\nfont-family:&quot;Times New Roman&quot;,serif;mso-fareast-font-family:&quot;Times New Roman&quot;;\nmso-fareast-theme-font:minor-fareast;mso-ansi-language:RU;mso-fareast-language:\nRU;mso-bidi-language:AR-SA\">Комплекс типового лабораторного оборудования\n«Распределение сети систем электроснабжения»</span>"
+        },
+        {
+          "yaw": 0.6969305100704961,
+          "pitch": -0.027230719571221584,
+          "title": "Нажмите для просмотра информации",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.030750576680159,
+          "pitch": 0.3099533568444013,
+          "title": "Рассказ о лабаратории электоэнергетики",
+          "text": "<span style=\"font-size:14.0pt;line-height:115%;color:#1F497D;>АУЕ ТАТАРЫ</span>" 
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
